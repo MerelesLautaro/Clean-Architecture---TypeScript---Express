d8fd757ef87def488da1a65c39f40e029820190a
Mereles Lautaro <lautaro.jobs@hotmail.com>
2025-06-02 15:14:32 -0300

    add dto and aditional class for register users

---
+++ src/application/dtos/UserCreatorDTO.ts
@@ -0,0 +1,7 @@
+export interface CreateUserDTO {
+  name?: string
+  username: string
+  age?: number
+}
+
+export {}
---
+++ src/domain/entities/NewUser.ts
@@ -0,0 +1,7 @@
+export interface NewUser {
+  name?: string
+  username?: string
+  age?: number
+}
+
+export {}
